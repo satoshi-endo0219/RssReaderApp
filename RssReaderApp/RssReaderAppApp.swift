129898e86ddcc90c1624c60d2b72ab37e85050f4
satoshi endo <endo-s@illmatics.co.jp>
2023-11-02 11:14:54 +0900

    Initial Commit

---
+++ RssReaderApp/RssReaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RssReaderAppApp.swift
+//  RssReaderApp
+//
+//  Created by satoshi on 2023/11/02.
+//
+
+import SwiftUI
+
+@main
+struct RssReaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
