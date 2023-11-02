f36f33f57912afd23308eb393e784635d7bddd7a
satoshi-endo0219 <satoshi.3104.endo@gmail.com>
2023-11-02 17:35:08 +0900

    RSSフィード選択画面の実装

---
+++ RssReaderApp/Const/Const.swift
@@ -0,0 +1,33 @@
+//
+//  Const.swift
+//  RssReaderApp
+//
+//  Created by satoshi on 2023/11/02.
+//
+
+import Foundation
+
+class Const {
+    // MARK: - SplashScreem
+    static let SplashText = "RSSReaderApp";
+
+    // MARK: - SelectRssFeedView
+    static let topPics = "主要";
+    static let topPicsXML = "https://news.yahoo.co.jp/rss/topics/top-picks.xml";
+    static let domestic = "国内";
+    static let domesticXML = "https://news.yahoo.co.jp/rss/topics/domestic.xml";
+    static let world = "国際";
+    static let worldXML = "https://news.yahoo.co.jp/rss/topics/world.xml";
+    static let business = "経済";
+    static let businessXML = "https://news.yahoo.co.jp/rss/topics/business.xml";
+    static let entertainment = "エンターテイメント";
+    static let entertainmentXML = "https://news.yahoo.co.jp/rss/topics/entertainment.xml";
+    static let sports = "スポーツ";
+    static let sportsXML = "https://news.yahoo.co.jp/rss/topics/sports.xml";
+    static let it = "IT";
+    static let itXML = "https://news.yahoo.co.jp/rss/topics/it.xml";
+    static let science = "科学";
+    static let scienceXML = "https://news.yahoo.co.jp/rss/topics/science.xml";
+    static let local = "地域";
+    static let localXML = "https://news.yahoo.co.jp/rss/topics/local.xml";
+}
