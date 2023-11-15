5a3ec22ca3ed010eeadbb52d5aacc5fd7b8910bf
satoshi-endo0219 <satoshi.3104.endo@gmail.com>
2023-11-15 15:37:37 +0900

    記事詳細でシェア機能を実装

--- RssReaderApp/View/DetailNewsView.swift
+++ RssReaderApp/View/DetailNewsView.swift
@@ -34,27 +34,49 @@ struct WebView: UIViewRepresentable {
 }
 
 struct DetailNewsView: View {
+    enum TabSelection {
+        case none
+        case addFavorite
+        case share
+    }
+    @State private var selection: TabSelection = .none
     @ObservedObject private var favoriteFeedDataViewModel = FavoriteFeedDataViewModel()
     @Environment(\.managedObjectContext)private var context
     @State var isFavorite: Bool = false
+    @State private var isPresentActivityController = false
     var newsItem: NewsItem?
     var body: some View {
         WebView(loadUrl: newsItem?.link ?? "")
-        Button(action: {
-            guard let newsItem = newsItem else {
-                print("newsItem is nil")
-                return
+        HStack() {
+            Button(action: {
+                guard let newsItem = newsItem else {
+                    print("newsItem is nil")
+                    return
+                }
+                self.isFavorite.toggle()
+                favoriteFeedDataViewModel.newsItem = newsItem
+                favoriteFeedDataViewModel.writeData(context: context)
+            }, label: {
+                if self.isFavorite {
+                    Text(Const.detailCancellRegisteFavorite)
+                } else {
+                    Text(Const.detailRegisterFavorite)
+                }
+            })
+            Button(action: {
+                isPresentActivityController.toggle()
+            }, label: {
+                Text("シェア")
+            })
+            .sheet(isPresented: $isPresentActivityController) {
+                if let newsItem = newsItem, let shareUrl = URL(string: newsItem.link) {
+                    ActivityView(activityItems: [newsItem.title, shareUrl],
+                                 applicationActivities: nil)
+                    .presentationDetents([.medium, .large])
+                }
             }
-            self.isFavorite.toggle()
-            favoriteFeedDataViewModel.newsItem = newsItem
-            favoriteFeedDataViewModel.writeData(context: context)
-        }, label: {
-            if self.isFavorite {
-                Text(Const.detailCancellRegisteFavorite)
-            } else {
-                Text(Const.detailRegisterFavorite)
-            }
-        })
+
+        }
         .frame(maxWidth: .infinity, maxHeight: 44)
         .background(Color(UIColor.lightGray))
         .onAppear() {
@@ -75,5 +97,20 @@ struct DetailNewsView: View {
 struct DetailNewsView_preview: PreviewProvider {
     static var previews: some View {
         DetailNewsView()
+    }
+}
+
+struct ActivityView: UIViewControllerRepresentable {
+
+    let activityItems: [Any]
+    let applicationActivities: [UIActivity]?
+
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        let controller = UIActivityViewController(activityItems: activityItems,
+                                                  applicationActivities: applicationActivities)
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
     }
 }
